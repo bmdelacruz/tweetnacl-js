f563efbbe5ec9c3b4356a3baf443816bd1ba86da
Bryan Dela Cruz <bryanmdlx@gmail.com>
2022-03-29 08:55:55 +0800

    Updated fast and notfast lowlevel index.ts files
    - Functions and constants are now explicitly exported

--- src/notfast/lowlevel/index.ts
+++ src/notfast/lowlevel/index.ts
@@ -1,19 +1,75 @@
-export * from './core.js';
-export * from './stream.js';
-export * from './onetimeauth.js';
-export * from './secretbox.js';
-export * from './scalarmult.js';
-export * from './box.js';
-export * from './hash.js';
-export * from './sign.js';
-export * from './scalar.js';
+export {
+    crypto_core_hsalsa20,
+} from './core.js';
 
-export { add } from './add.js';
-export { pow2523 } from './pow2523.js';
-export { pack25519, unpack25519, set25519, A, M, S, Z } from './25519.js';
+export {
+    crypto_stream_xor,
+    crypto_stream,
+    crypto_stream_salsa20_xor,
+    crypto_stream_salsa20,
+} from './stream.js';
 
-export * from '../../common/lowlevel/sizes.js';
-export * from '../../common/lowlevel/verify.js';
+export {
+    crypto_onetimeauth,
+    crypto_onetimeauth_verify,
+} from './onetimeauth.js';
+
+export {
+    crypto_verify_16,
+    crypto_verify_32,
+} from '../../common/lowlevel/verify.js';
+
+export {
+    crypto_secretbox,
+    crypto_secretbox_open,
+} from './secretbox.js';
+
+export {
+    crypto_scalarmult,
+    crypto_scalarmult_base,
+} from './scalarmult.js';
+
+export {
+    crypto_box_beforenm,
+    crypto_box_afternm,
+    crypto_box,
+    crypto_box_open,
+    crypto_box_keypair,
+} from './box.js';
+
+export {
+    crypto_hash,
+} from './hash.js';
+
+export {
+    crypto_sign,
+    crypto_sign_keypair,
+    crypto_sign_open,
+} from './sign.js';
+
+export {
+    crypto_secretbox_KEYBYTES,
+    crypto_secretbox_NONCEBYTES,
+    crypto_secretbox_ZEROBYTES,
+    crypto_secretbox_BOXZEROBYTES,
+    crypto_scalarmult_BYTES,
+    crypto_scalarmult_SCALARBYTES,
+    crypto_box_PUBLICKEYBYTES,
+    crypto_box_SECRETKEYBYTES,
+    crypto_box_BEFORENMBYTES,
+    crypto_box_NONCEBYTES,
+    crypto_box_ZEROBYTES,
+    crypto_box_BOXZEROBYTES,
+    crypto_sign_BYTES,
+    crypto_sign_PUBLICKEYBYTES,
+    crypto_sign_SECRETKEYBYTES,
+    crypto_sign_SEEDBYTES,
+    crypto_hash_BYTES,
+} from '../../common/lowlevel/sizes.js';
 
 export { gf, D } from '../../common/lowlevel/gf.js';
+export { pack25519, unpack25519, set25519, M, A, S, Z } from './25519.js';
+export { pow2523 } from './pow2523.js';
+export { add } from './add.js';
 export { modL, L } from '../../common/lowlevel/modL.js';
+export { scalarmult, scalarbase } from './scalar.js';
